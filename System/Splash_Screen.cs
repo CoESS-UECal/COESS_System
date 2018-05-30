a1ec28f9f8a06cdb377c051a9b0ebb8976ced6b4
cjmananquil <cjmananquil@hotmail.ph>
2018-05-30 23:11:28 +0800

    CHECK SPLASHSCREEN.CS LINE 55

--- System/Splash_Screen.cs
+++ System/Splash_Screen.cs
@@ -52,6 +52,7 @@ namespace System
                 if (pname.Length > 0)
                 {
                     MessageBox.Show("MySQL Server Started");
+                    //IT FUCKING WORKED
                     Form login = new Login();
                     login.Show();
                     this.Hide();
