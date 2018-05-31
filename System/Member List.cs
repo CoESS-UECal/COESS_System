cccd951649abc1fb78bf25fd0bee9de530cab77d
TeaRX27 <cursedx126@gmail.com>
2018-05-31 12:34:43 +0800

    >removed branch testing comments

--- System/Member List.cs
+++ System/Member List.cs
@@ -151,7 +151,7 @@ namespace System
             form1.ShowDialog();
             textBox4.Text = GeneralEdit.data;
         }
-//TESTING dsfjfajkgdksahjfjasjkfgdkfj
+
         private void textBox5_MouseDoubleClick(object sender, MouseEventArgs e)
         {
             GeneralEdit.data = textBox5.Text;
@@ -160,7 +160,7 @@ namespace System
             form1.ShowDialog();
             textBox5.Text = GeneralEdit.data;
         }
-        // test for branching
+        
         private void textBox1_MouseDoubleClick(object sender, MouseEventArgs e)
         {
             Form form1 = new NameEdit();
