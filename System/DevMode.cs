92b18f61116a9aac811d453e47096ee90d2ec805
cjmananquil <cjmananquil@hotmail.ph>
2019-11-11 12:13:35 +0800

    >Added Devmode.cs to Mainmenu redirection issue

--- System/DevMode.cs
+++ System/DevMode.cs
@@ -193,8 +193,8 @@ namespace System
         }
         private void button3_Click(object sender, EventArgs e)
         {
-                Form members = new Members();
-                members.Show();
+                Form mainmenu = new MainMenu();
+                mainmenu.Show();
                 Close();
         }
 
